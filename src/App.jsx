7b16a0981b5d637f6170968bf896e87bf499f343
Vedant Malaikar <vmalaikar@gmail.com>
2025-08-29 16:26:06 +0530

    added gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import {  Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Appointments from "./pages/Appointments";
 import Users from "./pages/Users";
 import AddAppointments from "./pages/AddAppointments";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <nav className="p-4 bg-blue-500 text-white flex gap-4">
         <Link to="/">Appointments</Link>
         <Link to="/users">Users</Link>
@@ -18,7 +18,7 @@ function App() {
         <Route path="/addappointment" element={<AddAppointments />} />
         {/* Add Users, Roles, Status pages here */}
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
