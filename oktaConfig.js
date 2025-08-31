90412e0313f42691fba85a1d18a7af83d0087770
Vedant Malaikar <vmalaikar@gmail.com>
2025-08-31 14:23:01 +0530

    test

--- oktaConfig.js
+++ oktaConfig.js
@@ -1,9 +1,10 @@
 const oktaConfig = {
   clientId: "0oauz7e4doGHfwIiW697",
   issuer: "https://integrator-1178574.okta.com/oauth2/default",
-  redirectUri: "https://vaccinereminder.netlify.app/login/callback",
-  scopes: ["openid", "profile", "email", "groups"],
+  redirectUri: window.location.origin + "/login/callback",
+  postLogoutRedirectUri: window.location.origin,       // redirects to home after logout
   pkce: true,
+  scopes: ["openid", "profile", "email", "groups"],
 };
 
 export default oktaConfig;
